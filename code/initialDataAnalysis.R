184df2fa8fd67bdb16328eb1d8e30c4b809896ff
Spencer Fox <spencer.fox85@gmail.com>
2016-01-06 17:16:47 -0600

    sdf

--- code/initialDataAnalysis.R
+++ code/initialDataAnalysis.R
@@ -4,7 +4,7 @@ if(grepl('spencerfox', Sys.info()['login'])) setwd('~/projects/flu/pandemic_dyna
 if(grepl('vagrant', Sys.info()['user'])) setwd('/vagrant/flu/pandemic_dynamics/code')
 if(grepl('sjf826', Sys.info()['login'])) setwd('/home1/02958/sjf826/pandemic_dynamics/code')
 if(grepl('tacc', Sys.info()['nodename'])) setwd('/home1/02958/sjf826/pandemic_dynamics/code')
-if(grepl('aksharaanand', Sys.info()['login'])) setwd('~/Desktop/epidemiology/pandemic_dynamics/code')
+if(grepl('aksharaanand', Sys.info()['login'])) setwd('~/pandemic_dynamics/code')
 
 ## Load up the R packages that will be used in the code
 library(ggplot2)
@@ -26,7 +26,7 @@ temp2 <- melt(sxtyDat, id.vars = c("WEEK", "YEAR", "date"))
 temp2$state <- sapply(strsplit(as.character(temp2$variable), split = "..", fixed=T), "[", 2)
 temp2$WEEK <- temp2$YEAR <- temp2$variable <-NULL
 
-## Add up all of the weekly deaths by city into the state
+## Add up all of the weekly deaths by city into the individual state
 stateData <- ddply(temp2, .variables = .(date, state), .fun =  function(x) sum(x$value, na.rm=T))
 ## removes temporary data frame
 rm(temp2)
