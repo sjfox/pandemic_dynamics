2620e0924e2e300b37d069c273e30f5023ee5b5d
Akshara Anand <aksharasanand@gmail.com>
2017-04-17 10:31:36 -0500

    Update updated_fxns.r

--- code/updated_fxns.r
+++ code/updated_fxns.r
@@ -21,9 +21,13 @@ temp2 <- melt(pdmData, id.vars = c("date", "fluYear"))
 temp2$state <- sapply(strsplit(as.character(temp2$variable), split = "..", fixed=T), "[", 2)
 temp2$variable <-NULL
 
+## Add up all of the weekly deaths by city into the individual state --> need to edit
+stateData <- ddply(temp2, .variables = .(date, state), 
+                   .fun =  function(x) data.frame(deaths=sum(x$value, na.rm=T), fluYear=x$fluYear[1]))
+
 #convert data to 4 columns
 year <- "year"
 week <- "week"
 state <- "state"
+deaths <- "stateData"
 data_long <- gather_(pdmData, year, week, state, deaths, factor_key =TRUE)
-
