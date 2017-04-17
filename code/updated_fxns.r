3d58ef79e01ca08972ac315c4ae4cf4f72798506
Akshara Anand <aksharaanand@aksharas-mbp.lbj.austinisd.org>
2017-04-17 10:29:00 -0500

    Converting the data into 4 columns after combining the cities into the states

--- code/updated_fxns.r
+++ code/updated_fxns.r
@@ -1,1 +1,29 @@
 #Converting wide-format data to long-format 
+# Initial analysis for pandemic influenza data
+rm(list=ls())
+if(grepl('spencerfox', Sys.info()['login'])) setwd('~/projects/flu/pandemic_dynamics/code')
+if(grepl('vagrant', Sys.info()['user'])) setwd('/vagrant/flu/pandemic_dynamics/code')
+if(grepl('sjf826', Sys.info()['login'])) setwd('/home1/02958/sjf826/pandemic_dynamics/code')
+if(grepl('tacc', Sys.info()['nodename'])) setwd('/home1/02958/sjf826/pandemic_dynamics/code')
+if(grepl('aksharaanand', Sys.info()['login'])) setwd('~/pandemic_dynamics/code')
+
+## Load up the R packages that will be used in the code
+library(ggplot2)
+library(reshape2)
+library(plyr)
+library(cowplot)
+library(tidyr)
+
+source("analysis_fxns.r")
+
+## Combine cities into states
+temp2 <- melt(pdmData, id.vars = c("date", "fluYear"))
+temp2$state <- sapply(strsplit(as.character(temp2$variable), split = "..", fixed=T), "[", 2)
+temp2$variable <-NULL
+
+#convert data to 4 columns
+year <- "year"
+week <- "week"
+state <- "state"
+data_long <- gather_(pdmData, year, week, state, deaths, factor_key =TRUE)
+
